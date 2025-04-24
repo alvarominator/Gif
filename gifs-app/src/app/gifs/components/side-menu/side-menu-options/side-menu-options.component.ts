9b5f44970f8aab60ac8d2866eb0284cc50992015
alvarominator <alvarisimo@hotmail.com>
2025-04-24 09:55:08 +0200

    Fin S6

---
+++ gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+interface MenuOption{
+  icon: string;
+  label: string;
+  sublabel: string;
+  route: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [
+    RouterLink, RouterLinkActive
+  ],
+  templateUrl: './side-menu-options.component.html',
+})
+export class SideMenuOptionsComponent { 
+  menuOptions: MenuOption [] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label:'Trending',
+      sublabel:'Gifs Populares',
+      route:'/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label:'Buscador',
+      sublabel:'Buscar Gifs',
+      route:'/dashboard/search',
+    }
+  ]
+
+
+
+
+}
